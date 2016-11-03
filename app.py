129352d0c8bd68a12d1982758ead835c8da5f364
jamiieryan <g00317177@gmit.ie>
2016-11-03 11:31:07 +0000

    menu & templates working

--- app.py
+++ app.py
@@ -2,14 +2,28 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hell():
+def main():
 	return render_template("index.html")
 
-@app.route("/hello/")
-@app.route("/hello/<name>", methods = ['GET'])
-def hello(name=None):
-	name = request.args.get("name")
-	return render_template("hello.html", name=name)
+@app.route("/route2")
+def route2():
+	return render_template("route2.html")
+
+@app.route("/route3")
+def route3():
+	return render_template("route3.html")
+
+@app.route("/route4")
+def route4():
+	return render_template("route4.html")
+
+
+
+#@app.route("/hello/")
+#@app.route("/hello/<name>", methods = ['GET'])
+#def hello(name=None):
+#	name = request.args.get("name")
+#	return render_template("hello.html", name=name)
 	
 
 
