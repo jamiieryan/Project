a9f1230329b5563076dc5d83d527a121ec97a0da
jamiieryan <g00317177@gmit.ie>
2016-11-07 09:26:12 +0000

    static index and ajax.load() functioning

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
+
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-	return render_template("index.html")
+
+
+# route for handling the login page logic
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+
+@app.route("/index")
+def index():
+	return app.send_static_file("index.html")
 
 @app.route("/route2")
 def route2():
--- static/index.js
+++ static/index.js
@@ -1,4 +1,36 @@
+//fullscreen menu toggle
 $('#toggle').click(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
   });
+
+
+
+
+
+
+
+
+//fadeIn route2 div
+$(document).ready(function(){
+   
+    
+    //$("#anchor").click(function(event) {});
+    
+    
+    //based on #anchor click
+    $("#anchor").click(function(){
+        
+        $("#pre").slideUp(500);
+        //$("#pre").animate({width:'toggle'},500);
+        
+        
+        //hides what's inside selected div and replaces it with item to be loaded 
+        $("#main").load("/route2",function(){}).hide().delay( 750 ).slideDown(750).fadeIn(1500);
+        
+    });
+    
+    
+    
+    
+});
